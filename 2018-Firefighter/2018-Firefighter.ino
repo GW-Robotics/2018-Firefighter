13f43e9e046a8f4098e38248c3fc92df40e2bf3f
Alex Broaddus <alexbroaddus1@gmail.com>
2017-10-22 15:21:20 -0400

    Added left ultrasonic sensor and removed initial movement code
    I added the pins and setup for the left ultrasonic sensor that was added to the robot. I also removed the initial example movement code we put in our loop on the first day.

--- 2018-Firefighter/2018-Firefighter.ino
+++ 2018-Firefighter/2018-Firefighter.ino
@@ -9,12 +9,15 @@
 
 #define FRONT_US_ECHO   2
 #define FRONT_US_TRIG   3
+#define LEFT_US_ECHO   18
+#define LEFT_US_TRIG   19
 
 Servo leftServo;
 Servo rightServo;
 Servo extinguisher;
 
 Ultrasonic frontUltrasonic(FRONT_US_ECHO, FRONT_US_TRIG, true);
+Ultrasonic leftUltrasonic(LEFT_US_ECHO, LEFT_US_TRIG, true);
 
 void moveForward() {
   leftServo.write(180);
@@ -84,13 +87,7 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-//  if (frontUltrasonic.getDistance() > 3) {
-//    moveForward();
-//  } else {
-//    moveBackward();
-//  }
+// put your main code here, to run repeatedly:
   
   extinguishFire();
   
