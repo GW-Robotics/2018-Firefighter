4a099536ded5628bdc6cb62a8d39803b7bd72e25
Rick Sear <yobob.sear@gmail.com>
2018-04-05 21:57:33 -0400

    Updated turn to 45

--- 2018-FirefighterL1/Left_Right_Follow.ino
+++ 2018-FirefighterL1/Left_Right_Follow.ino
@@ -63,12 +63,8 @@ void levelOneNav() {
   {
     //void lowerRightRoom(){
     //ADD TURN(0) TO MOVE FUNCTION
-    
-    turn(-20); //turn the robot 45, but since it's so close to the wall, this needs to happen gradually
-    rollForward();
-    delay(1000);
-    stopRobot();
-    turn(-25);
+
+    turn(-45);
 
     //moveForward(diagonalMove);  //this function is located on Alex's laptop, which is currently dead
 
